f42c7d4f556bb84277dbd41bffc4ea4cc640858d
RenLovina <renataalvespereira82@gmail.com>
2025-06-11 14:28:02 -0300

    Rename GerenciadorPetshop.java.class to GerenciadorPetshop.java

--- GerenciadorPetshop.java
+++ GerenciadorPetshop.java
@@ -7,32 +7,32 @@ public class GerenciadorPetshop {
         Gato cat = new Gato("Mimi", "Persa", 3, "Ana", "azuis");
         OutroAnimal coelho = new OutroAnimal("Bunny", "Mini Rex", 2, "Lucas", "coelho");
 
-        // Criando serviços
+        // Criando serviÃ§os
         Banho banho = new Banho(30);
         Tosa tosa = new Tosa(40);
         ConsultaVeterinaria consulta = new ConsultaVeterinaria(100);
 
-        // Exibindo preços
+        // Exibindo preÃ§os
         System.out.println(dog);
-        System.out.println("Preço banho: R$ " + banho.calcularPreco(dog));
-        System.out.println("Preço tosa: R$ " + tosa.calcularPreco(dog));
-        System.out.println("Preço consulta: R$ " + consulta.calcularPreco(dog));
+        System.out.println("PreÃ§o banho: R$ " + banho.calcularPreco(dog));
+        System.out.println("PreÃ§o tosa: R$ " + tosa.calcularPreco(dog));
+        System.out.println("PreÃ§o consulta: R$ " + consulta.calcularPreco(dog));
 
         System.out.println();
 
         System.out.println(cat);
-        System.out.println("Preço banho: R$ " + banho.calcularPreco(cat));
-        System.out.println("Preço tosa: R$ " + tosa.calcularPreco(cat));
-        System.out.println("Preço consulta: R$ " + consulta.calcularPreco(cat));
+        System.out.println("PreÃ§o banho: R$ " + banho.calcularPreco(cat));
+        System.out.println("PreÃ§o tosa: R$ " + tosa.calcularPreco(cat));
+        System.out.println("PreÃ§o consulta: R$ " + consulta.calcularPreco(cat));
 
         System.out.println();
 
         System.out.println(coelho);
-        System.out.println("Preço banho: R$ " + banho.calcularPreco(coelho));
-        System.out.println("Preço tosa: R$ " + tosa.calcularPreco(coelho));
-        System.out.println("Preço consulta: R$ " + consulta.calcularPreco(coelho));
+        System.out.println("PreÃ§o banho: R$ " + banho.calcularPreco(coelho));
+        System.out.println("PreÃ§o tosa: R$ " + tosa.calcularPreco(coelho));
+        System.out.println("PreÃ§o consulta: R$ " + consulta.calcularPreco(coelho));
 
-        // Comparações
+        // ComparaÃ§Ãµes
         Cachorro dog2 = new Cachorro("Rex", "Labrador", 5, "Carlos", "grande");
         System.out.println("\nDog equals Dog2? " + dog.equals(dog2));
         System.out.println("HashCode Dog: " + dog.hashCode() + " | Dog2: " + dog2.hashCode());
